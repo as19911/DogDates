d036048a95ea28a00abfff9ac080db4bf0272040
Liza Yashin <liza.ya3011@gmail.com>
2022-02-20 16:47:50 -0600

    add title and introduction to home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,6 +6,13 @@ export default function Home(props) {
   return (
     <div className="home-page">
       <div className="home-page__content">
+        <h1>Dog Dates </h1>
+        <div className="home-page__introduction">
+          Want to find a play date for your dog?
+          <br></br>
+          Dog dates is the best way to find sogs in your area that are looking
+          for friends just like you!
+        </div>
         <div className="home-page__image">
           <img src={props.image} alt={props.name} />
         </div>
