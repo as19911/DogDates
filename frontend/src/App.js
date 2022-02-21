a3e6f1b3382d965e17a336923a3e3571b9725d5a
Liza Yashin <liza.ya3011@gmail.com>
2022-02-20 19:02:57 -0600

    Login page with email + password fields + install bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <MainNavigation />
       <main>
-        <Home
-          image="https://media-cldnry.s-nbcnews.com/image/upload/newscms/2020_28/1587661/dogs-age-years-kb-inline-200707.jpg"
-          name="homePageDogsImage"
-        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                image="https://media-cldnry.s-nbcnews.com/image/upload/newscms/2020_28/1587661/dogs-age-years-kb-inline-200707.jpg"
+                name="homePageDogsImage"
+              />
+            }
+          />
+
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </main>
     </Router>
   );
